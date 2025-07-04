bab0151f7593347df91217ab3ebb0bb6cfa58bce
Outlier <nzombeebenezer@gmail.com>
2025-07-04 17:14:58 +0100

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,27 @@
+import { AboutSection } from "../components/AboutMe";
+import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
-export const Home = ()=> {
-  return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-    {/* -----Theme Toggle----- */}
-    <ThemeToggle />
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* -----Theme Toggle----- */}
+      <ThemeToggle />
 
-    {/* ----Background Effects----- */}
-    <StarBackground/>
+      {/* ----Background Effects----- */}
+      <StarBackground />
 
+      {/* --navBar---- */}
+      <Navbar />
 
-    {/* --navBar---- */}
-    <Navbar/>
-
-
-    {/* ---Main Content---- */}
-
-
-    {/* ---Footer---- */}
-  </div>;
+      {/* ---Main Content---- */}
+      <main>
+        <HeroSection/>
+        <AboutSection/>
+      </main>
+      {/* ---Footer---- */}
+    </div>
+  );
 };
