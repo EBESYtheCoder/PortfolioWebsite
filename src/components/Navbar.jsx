673c30cf1e8b18053eda74b6b7fdfeeae8b25891
Outlier <nzombeebenezer@gmail.com>
2025-07-04 15:56:30 +0100

    desktop navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,15 +28,33 @@ export const Navbar = () => {
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
       )}
     >
-
       <div className="container flex items-center justify-between">
-        <a className="text-xl font-bold text-primary flex items-center">
+        <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#hero"
+        >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> ENTech </span> Portfolio
+            <span className="text-glow text-foreground"> ENTech </span>{" "}
+            Portfolio
           </span>
         </a>
+
+        {/* ----desktop nav */}
+
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* ----mobile nav */}
       </div>
-  
     </nav>
   );
 };
