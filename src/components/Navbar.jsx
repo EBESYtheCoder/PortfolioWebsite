42e38481e3212e020e1a462edec0160a3ae34535
Outlier <nzombeebenezer@gmail.com>
2025-07-04 15:32:22 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,6 +27,16 @@ export const Navbar = () => {
         "fixed w-full z-40 transition-all duration-300",
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
       )}
-    ></nav>
+    >
+
+      <div className="container flex items-center justify-between">
+        <a className="text-xl font-bold text-primary flex items-center">
+          <span className="relative z-10">
+            <span className="text-glow text-foreground"> ENTech </span> Portfolio
+          </span>
+        </a>
+      </div>
+  
+    </nav>
   );
 };
