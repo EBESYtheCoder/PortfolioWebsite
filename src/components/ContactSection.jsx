c545e89ec8b6b147133f541a2128f3a5ed8d8711
EBESYtheCoder <nzombeebenezer@gmail.com>
2025-08-22 21:52:58 +0100

    1

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -8,96 +8,96 @@ import {
   Twitter,
   TwitterIcon,
   X,
-} from "lucide-react";
-import { cn } from "../lib/utils";
-import { useToast } from "@/hooks/use-toast";
-import { useState } from "react";
+} from 'lucide-react';
+import { cn } from '../lib/utils';
+import { useToast } from '@/hooks/use-toast';
+import { useState } from 'react';
 
 export const ContactSection = () => {
   const { toast } = useToast();
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     setIsSubmitting(true);
 
     setTimeout(() => {
       toast({
-        title: "Message sent!",
+        title: 'Message sent!',
         description: "Thank you for your message. I'll get back to you soon.",
       });
       setIsSubmitting(false);
     }, 1500);
   };
   return (
-    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
-      <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold text-center">
-          Get In <span className="text-primary"> Touch</span>
+    <section id='contact' className='py-24 px-4 relative bg-secondary/30'>
+      <div className='container mx-auto max-w-5xl'>
+        <h2 className='text-3xl md:text-4xl font-bold text-center'>
+          Get In <span className='text-primary'> Touch</span>
         </h2>
-        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+        <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
           Have a project in mind or want to collaborate? Feel free to reach out.
           I'm always open to discussing new opportunities.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          <div className="space-y-8">
-            <h3 className="text-2xl font-semibold"> Contact Information</h3>
-            <div className="space-y-6 justify-center">
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <Mail className="H-6 W-6 text-primary" />
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+          <div className='space-y-8'>
+            <h3 className='text-2xl font-semibold'> Contact Information</h3>
+            <div className='space-y-6 justify-center'>
+              <div className='flex items-start space-x-4'>
+                <div className='p-3 rounded-full bg-primary/10'>
+                  <Mail className='H-6 W-6 text-primary' />
                 </div>
                 <div>
-                  <h4 className="font-medium"> Email</h4>
+                  <h4 className='font-medium'> Email</h4>
                   <a
-                    href="mailto:nzombeebenezer@gmail.com"
-                    className="text-muted-foreground hover:text-primary transition-colors"
+                    href='mailto:nzombeebenezer@gmail.com'
+                    className='text-muted-foreground hover:text-primary transition-colors'
                   >
                     nzombeebenezer@gmail.com
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <Phone className="H-6 W-6 text-primary" />
+              <div className='flex items-start space-x-4'>
+                <div className='p-3 rounded-full bg-primary/10'>
+                  <Phone className='H-6 W-6 text-primary' />
                 </div>
                 <div>
-                  <h4 className="font-medium"> Phone</h4>
+                  <h4 className='font-medium'> Phone</h4>
                   <a
-                    href="tel:+263562197794"
-                    className="text-muted-foreground hover:text-primary transition-colors"
+                    href='tel:+213562197794'
+                    className='text-muted-foreground hover:text-primary transition-colors'
                   >
-                    +263 562197794
+                    +213 562197794
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
-                <div className="p-3 rounded-full bg-primary/10">
-                  <MapPin className="H-6 W-6 text-primary" />
+              <div className='flex items-start space-x-4'>
+                <div className='p-3 rounded-full bg-primary/10'>
+                  <MapPin className='H-6 W-6 text-primary' />
                 </div>
                 <div>
-                  <h4 className="font-medium"> Location</h4>
-                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                  <h4 className='font-medium'> Location</h4>
+                  <a className='text-muted-foreground hover:text-primary transition-colors'>
                     Bab Ezzouar, Algiers, Algeria
                   </a>
                 </div>
               </div>
             </div>
-            <div className="pt-8">
+            <div className='pt-8'>
               <h4> Connect With Me</h4>
-              <div className="flex space-x-4 justify-center">
+              <div className='flex space-x-4 justify-center'>
                 <a
-                  href="https://www.linkedin.com/in/nzombeebenezer"
-                  target="_blank"
+                  href='https://www.linkedin.com/in/nzombeebenezer'
+                  target='_blank'
                 >
                   <Linkedin />
                 </a>
-                <a href="https://x.com/nzombe_ebenezer" target="_blank">
+                <a href='https://x.com/nzombe_ebenezer' target='_blank'>
                   <TwitterIcon />
                 </a>
                 <a
-                  href="https://www.instagram.com/mr.n.z.o.m.b.e_zw"
-                  target="_blank"
+                  href='https://www.instagram.com/mr.n.z.o.m.b.e_zw'
+                  target='_blank'
                 >
                   <Instagram />
                 </a>
@@ -105,71 +105,71 @@ export const ContactSection = () => {
             </div>
           </div>
           <div
-            className="bg-card p-8  rounded-lg shadoe-xs"
+            className='bg-card p-8  rounded-lg shadoe-xs'
             onSubmit={handleSubmit}
           >
-            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+            <h3 className='text-2xl font-semibold mb-6'> Send a Message</h3>
 
-            <form action="" className="space-y-6">
+            <form action='' className='space-y-6'>
               <div>
                 <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-2 "
+                  htmlFor='name'
+                  className='block text-sm font-medium mb-2 '
                 >
-                  {" "}
-                  Your Name{" "}
+                  {' '}
+                  Your Name{' '}
                 </label>
                 <input
-                  type="text"
-                  id="name"
-                  name="name"
+                  type='text'
+                  id='name'
+                  name='name'
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary/30"
-                  placeholder="Ebenezer Nzombe..."
+                  className='w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary/30'
+                  placeholder='Ebenezer Nzombe...'
                 />
               </div>
               <div>
                 <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-2 "
+                  htmlFor='name'
+                  className='block text-sm font-medium mb-2 '
                 >
-                  {" "}
-                  Your Email{" "}
+                  {' '}
+                  Your Email{' '}
                 </label>
                 <input
-                  type="email"
-                  id="email"
-                  name="email"
+                  type='email'
+                  id='email'
+                  name='email'
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary/30"
-                  placeholder="entech@gmail.com"
+                  className='w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary/30'
+                  placeholder='entech@gmail.com'
                 />
               </div>
               <div>
                 <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-2 "
+                  htmlFor='name'
+                  className='block text-sm font-medium mb-2 '
                 >
-                  {" "}
-                  Your Message{" "}
+                  {' '}
+                  Your Message{' '}
                 </label>
                 <textarea
-                  id="message"
-                  name="message"
+                  id='message'
+                  name='message'
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary/30 resize-none"
+                  className='w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary/30 resize-none'
                   placeholder="Hello, l'd like to talk about..."
                 />
               </div>
               <button
-                type="submit"
+                type='submit'
                 disabled={isSubmitting}
                 className={cn(
-                  "cosmic-button w-full flex items-center justify-center gap-2"
+                  'cosmic-button w-full flex items-center justify-center gap-2'
                 )}
               >
-                {" "}
-                {isSubmitting ? "Sending..." : "Send Message"}
+                {' '}
+                {isSubmitting ? 'Sending...' : 'Send Message'}
                 <Send size={16} />
               </button>
             </form>
