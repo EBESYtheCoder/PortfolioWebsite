45fb514a84357326196c0a48ccdf16360cfaef4c
EBESYtheCoder <nzombeebenezer@gmail.com>
2025-08-22 23:17:12 +0100

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
-import { Toaster } from "./components/ui/toaster";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { Toaster } from './components/ui/toaster';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -10,9 +11,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics /> {/* Add this line for Vercel Analytics */}
     </>
   );
 }
